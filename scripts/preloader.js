d660d393c95c2f981d8501e58c8eeb79a7f619c2
Eremeow138 <eremeow138@yandex.ru>
2024-06-20 21:18:17 +0800

    Revert "first commit"
    This reverts commit f6585a1e98ea9334ac69720745e82e420638336b.

--- scripts/preloader.js
+++
@@ -1,7 +0,0 @@
-//функция для загрузки прелодера с задержкой в полсекунды
-window.addEventListener('load', function () {
-    var preloader = document.getElementById('preloader');
-    setTimeout(function() {
-        preloader.style.display = 'none';
-    }, 2000); //задержка в миллисекундах
-});
